54b1a4061ab43147ac6e48d6d010c008f92977f7
Venia2446 <venia.koshevoi.eltech@gmail.com>
2021-11-23 17:31:30 +0300

    beta 2.0

--- FPS_Shooter_v1/Assets/Scripts/Bullet/Shotgun_bullet_collision.cs
+++ FPS_Shooter_v1/Assets/Scripts/Bullet/Shotgun_bullet_collision.cs
@@ -3,43 +3,58 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Shotgun_bullet_collision : MonoBehaviour
-{  
+{
+    GameObject text_score_alt_2_clone;
     private Shutting_Screept shr_scr_shootgun;
     private bool shotgun_ammo_check = true;
+    
     private Transform text_up;
     public GameObject Explosion;
     public GameObject text_score_alt_2;
+
+
+
+
 
     void Start()
     {
         GameObject sh_obj = GameObject.Find("Player/Main Camera/Gun");
         shr_scr_shootgun = sh_obj.GetComponent<Shutting_Screept>();
     }
+    //private void Update()
+    //{
+    //    if (text_score_alt_2_clone != null)
+    //    {
+    //        text_up.position += Vector3.up * 5f;
+    //        //text_up.Translate(Vector3.forward * 10f * Time.deltaTime);
+    //        Destroy(text_score_alt_2_clone, 2);
+    //    }
+    //}
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "green_target" || collision.gameObject.tag == "Target")
         {
             if (!shotgun_ammo_check) return;            
             shotgun_ammo_check = false;
-            shr_scr_shootgun.ammo_alt_up();            
-            GameObject text_score_alt_2_clone;         
+            shr_scr_shootgun.ammo_alt_up();                       
             text_score_alt_2_clone = Instantiate(text_score_alt_2, collision.transform.position, Quaternion.identity);
+            
+            
+            
             text_up = text_score_alt_2_clone.GetComponent<Transform>();
-            text_up.Translate(new Vector3(0, 1f*10,1f));
-
+            //text_up.position += Vector3.up * 10f;
+            //text_up.Translate(Vector3.forward * 10f * Time.deltaTime);
             Instantiate(Explosion, collision.transform.position, Quaternion.identity);
-            //text_up.transform.Translate(Vector3.up * 100 * Time.deltaTime);
-           
+
             Destroy(gameObject);
             Destroy(text_score_alt_2_clone, 2);
             Destroy(collision.gameObject);
         }
         if (collision.gameObject.name == "test_prefub") // заготовка под бомбу???
-
         {
             Debug.Log("hit");
         }
-        Destroy(gameObject);
+        Destroy(gameObject);       
     }
 }
 
---
+++ FPS_Shooter_v1/Assets/Scripts/text_scripts/text_move.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class text_move : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+
+
+
+    
+    private TextMesh text_color;
+    private float new_alpha;
+
+
+    private void Start()
+    {
+        
+        text_color = gameObject.GetComponent<TextMesh>();
+        
+
+    }
+    public float low_alpha() => new_alpha -= 0.004f;
+
+
+    void Update()
+    {
+        new_alpha = text_color.color.a;
+        low_alpha();
+        transform.Translate(Vector3.up * 0.8f * Time.deltaTime);
+        text_color.color = new Color(text_color.color.r, text_color.color.g, text_color.color.b, new_alpha);
+        
+        
+    }
+}
